03ee63fe1a04f7022035246ef714a4bdb452cb78
BILLY <104698069+beanboi64@users.noreply.github.com>
2025-07-15 13:42:07 +0200

    Update app.py

--- exercises/07-Create-a-Basic-HTML/app.py
+++ exercises/07-Create-a-Basic-HTML/app.py
@@ -1,13 +1,16 @@
-a = '</title>'
-b = '</html>'
-c = '<head>'
-d = '</body>'
-e = '<html>'
-f = '</head>'
-g = '<title>'
-h = '<body>'
+a = '</title>' #4
+b = '</html>' #8
+c = '<head>' #2
+d = '</body>' #7
+e = '<html>' #1
+f = '</head>' #5
+g = '<title>' #3
+h = '<body>' #6
 
 # ❌ ⬆ DON'T CHANGE THE CODE ABOVE ⬆ ❌
 
 # ✅ ↓ start coding below here ↓ ✅
 
+html_document = e + c + g + a + f + h + d + b
+
+print(html_document)
